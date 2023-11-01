64e5bbe390163bdb5f35d5dd72fdb0005d695953
Marchi8ni <marchi8ni@gmail.com>
2023-11-01 23:03:18 +0100

    Primary and secondary chart was created

--- t_Marek_Lestina_project_SQL_primary_final.sql
+++ t_Marek_Lestina_project_SQL_primary_final.sql
@@ -1,1 +1,40 @@
 -- Tvorba tabulky
+-- part ONE - PAYROLL
+
+CREATE TABLE IF NOT EXISTS t_marek_lestina_primary_part1 AS
+SELECT
+cp.payroll_year,
+round(avg(cp.value)) AS avg_wages,
+cpib.name AS industry_name, 
+cp.industry_branch_code
+FROM czechia_payroll cp
+LEFT JOIN czechia_payroll_industry_branch cpib 
+	ON cpib.code = cp.industry_branch_code 
+WHERE cp.value IS NOT NULL
+AND cp.value_type_code = 5958
+AND cp.industry_branch_code IS NOT NULL 
+GROUP BY cp.industry_branch_code, cp.payroll_year
+ORDER BY cp.payroll_year ASC; 
+
+-- Part 2 - PRICE
+CREATE TABLE IF NOT EXISTS t_marek_lestina_primary_part2 AS
+SELECT
+ROUND(AVG(cp.value)) AS food_price,
+cp.category_code AS Food_category,
+YEAR(cp.date_from) AS price_year,
+cpc.name AS food_name,
+cpc.price_value,
+cpc.price_unit 
+FROM czechia_price cp
+LEFT JOIN czechia_price_category cpc 
+	ON cpc.code = cp.category_code 
+GROUP BY cp.category_code, price_year  
+ORDER BY cp.date_from ASC;
+
+
+-- tabulka PRIMARY
+CREATE TABLE IF NOT EXISTS t_Marek_Lestina_project_SQL_primary_final
+SELECT *
+FROM t_marek_lestina_primary_part1 tmlpp
+JOIN t_marek_lestina_primary_part2 tmlpp2 
+	ON tmlpp2.price_year = tmlpp.payroll_year; 
--- t_Marek_Lestina_project_SQL_secondary_final.sql
+++ t_Marek_Lestina_project_SQL_secondary_final.sql
@@ -1,1 +1,17 @@
--- dodatečná data o dalších evropských státech
+/*
+ * SECONDARY TABULKA projekt -- dodatečná data o dalších evropských státech
+ * */
+CREATE TABLE IF NOT EXISTS t_Marek_Lestina_project_SQL_secondary_final
+SELECT
+c.country,
+c.population,
+e.`year`,
+e.GDP ,
+e.gini 
+FROM countries c
+JOIN economies e
+ON e.country = c.country 
+WHERE 
+c.continent = 'Europe'
+AND  e.`year` BETWEEN 2006 and 2018
+ORDER BY e.`year` ASC; 
